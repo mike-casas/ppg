19e59e50bd9ae0bc8fe1f8eea57a33a9a21ce3ef
mike-casas <neneriostb@gmail.com>
2015-01-18 02:35:03 -0500

    add alias to relationchip between models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  belongs_to :user
-  has_and_belongs_to_many :users
+  belongs_to :owner , class_name: "User"
+  has_and_belongs_to_many :contributors ,class_name: "User"
 end
