cdb28cba4f023dcf3f37ff51006183a6756d5fc4
mike-casas <neneriostb@gmail.com>
2015-01-20 19:50:05 -0500

    personalize dashboard projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,31 @@
 module ProjectsHelper
+
+  def list_project(project,state)
+
+ if state== 'invited'
+
+    html = <<-HTML
+    <div class="col-lg-3  content-project">
+      <p>#{project.name}</p>
+    </div>
+   HTML
+
+  link_to html.html_safe ,project_path(project)
+
+
+  else
+      html = <<-HTML
+    <div class="col-lg-3  content-project">
+      <p>#{project.name}</p>
+          <i class='fa fa-fw fa-user icon-dashboard'></i>
+        <a id="trash" data-confirm="Deseas eliminar este registro" rel="nofollow" data-method="delete" href="/projects/#{project.id}">
+          <i class='fa fa-fw fa-trash icon-dashboard'></i></a>
+   </div>
+   HTML
+
+    link_to html.html_safe ,project_path(project)
+
+ end
+
+  end
 end
